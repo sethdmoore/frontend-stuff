51d80a3973a6eb3adcd3226fa915b0b29fcfa71f
Seth Moore <sethdmoore@github.com>
2015-08-04 02:18:49 -0400

    broke everything

--- api.py
+++ api.py
@@ -1,25 +1,35 @@
 #!/usr/bin/env python
 
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request, Response
 import json
+import random
 from flask.ext.cors import CORS, cross_origin
 
 app =  Flask("API")
 #cors = cross_origin(app, resources={r"/": {"origins": "*"}})
 cors = CORS(app)
 
-@app.route("/")
+mlist = [
+        {"name": ["bill", "morrison"],
+         "occupations": ["burger tuner"]},
+        {"name": ["candy", "carolsen"],
+         "occupations": ["developer", "chief clock watcher"]},
+        {"name": ["jeff", "jeffries"],
+         "occupations": ["spoon licker", "brown noser"]}
+]
+
+@app.route("/", methods=['GET', 'PUT'])
 def index():
-    mlist = [
-            {"name": ["bill", "morrison"],
-             "occupations": ["burger tuner"]},
-            {"name": ["candy", "carolsen"],
-             "occupations": ["developer", "chief clock watcher"]},
-            {"name": ["jeff", "jeffries"],
-             "occupations": ["spoon licker", "brown noser"]}
-        ]
     # return json.dumps({["Bill", "Morrison"], ["Candy", "Carolsen"]})
-    return jsonify({"members": mlist})
+    if request.method == "GET":
+        return jsonify({"members": mlist})
+    else:
+        if mlist:
+            del mlist[random.randint(0, len(mlist) -1)]
+            return jsonify({"status": "done"})
+        else:
+            return Response(jsonify({"status": "can't do"}), status=406)
+
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', debug=True)
